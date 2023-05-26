1c04bd80b0c052a1806f91e8cb7932eca3ec067b
MichaelMantovani <michmantovani@gmail.com>
2023-05-26 15:23:39 +0200

    Set age limit and price

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log('JS OK')
+
+
+
+/* OPERAZIONI PRELIMINARI */
+
+
+// TODO Recupero elemento dal DOM
+
+// Stabilisco tariffe di prezzo e sconti per età
+
+// Percentuale di sconto per maggiorenni
+const discountAdult = 20;
+
+// Percentuale di sconto per over65
+const discountSenior = 40;
+
+// Soglie di età
+const adultAge = 18;
+const seniorAge= 65;
+
+// Prezzo al km
+const kmPrice = 0.21; 
+
+
+
+
+
